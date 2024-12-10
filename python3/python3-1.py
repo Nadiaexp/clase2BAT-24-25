62f57101537ca03aef70030a0d38b1b059f2cab3
nadiaexp <nadexpmoy@alu.edu.gva.es>
2024-12-10 09:48:50 +0100

    IES10D

---
+++ python3/python3-1.py
@@ -0,0 +1,7 @@
+# nombre = input("¿Cúal es tu nombre y apellido? ") 
+# apellido = input("¿Cuál es tu nombre y apellido? ")
+# print("Hola Sra. ", apellido)
+# print("Hola Sra. ", nombre)
+nombre = input("¿Cuál es tu nombre? ")
+apellido = input("¿Cuál es tu apellido? ")
+print("Hola Sra. ", nombre , apellido)
