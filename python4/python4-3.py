f403f2ef4a08130c88cd66e678ba1970a0de249f
nadiaexp <nadexpmoy@alu.edu.gva.es>
2024-12-12 10:15:12 +0100

    IES12D

---
+++ python4/python4-3.py
@@ -0,0 +1,8 @@
+n1 = int(input("Dame un número entero: "))
+n2 = int(input("Dame otro número entero: "))
+
+if n2 == 0 :
+    print("No se puede dividir entre 0")
+
+else :
+    print("La división del primero entre el segundo es: ", n1 / n2)
