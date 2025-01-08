1baa894e44793457cbbcd450f515a2d9970a3d53
nadiaexp <nadexpmoy@alu.edu.gva.es>
2025-01-08 13:27:57 +0100

    IES8E

---
+++ python5/python5-1.py
@@ -0,0 +1,8 @@
+n = int(input("Dame un número: "))
+
+if n > 0 :
+    print("Es positivo")
+elif n < 0 :
+    print("Es negativo")
+elif n == 0 :
+    print("Es cero")
---
+++ python5/python5-2.py
@@ -0,0 +1,26 @@
+n = int(input("Dame un número entre el 1 y el 12: "))
+
+if n == 1 :
+    print("Enero")
+elif n == 2 :
+    print("Febrero")
+elif n == 3 :
+    print("Marzo")
+elif n == 4 :
+    print("Abril")
+elif n == 5 :
+    print("Mayo")
+elif n == 6 :
+    print("Junio")
+elif n == 7 :
+    print("Julio")
+elif n == 8 :
+    print("Agosto")
+elif n == 9 :
+    print("Septiembre")
+elif n == 10 :
+    print("Octubre")
+elif n == 11 :
+    print("Noviembre")
+elif n == 12 :
+    print("Diciembre")
