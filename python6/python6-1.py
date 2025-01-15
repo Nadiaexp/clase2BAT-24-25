25ac19b696f42407ce2ebe36ca28e649ecbff8e8
nadiaexp <nadexpmoy@alu.edu.gva.es>
2025-01-15 13:46:54 +0100

    IES15E

--- python6/python6-1.py
+++ python6/python6-1.py
@@ -1,1 +1,7 @@
 #Programa que calcula la suma de 135 y 768
+suma = 905
+n = int(input("Dame la suma de 135 y 768: "))
+
+while (n != 905) :
+    n = int(input("Dame otro número: "))
+print("¡Conseguido!")
---
+++ python6/python6-2.py
@@ -0,0 +1,56 @@
+"""
+#Programa que calcula la suma de 135 y 768
+suma = 905
+n = int(input("Dame la suma de 135 y 768: "))
+
+while (n != 905) :
+    n = int(input("Dame otro número: "))
+print("¡Conseguido!")
+
+
+
+code = int(input("Dame un código: "))
+
+while (code != "admin") :
+    code = int(input("Código erróneo, inténtalo de nuevo: "))
+    
+
+clave = int(input("Dame una contraseña de 4 números: "))
+
+while (clave != "0987") :
+    clave = int(input("Clave errónea, inténtalo de nuevo: "))
+
+
+
+
+#Programa que calcula la división entre dos números
+dividendo = int(input("Dividendo: "))
+divisor = int(input("Divisor: "))
+
+while divisor == 0 :
+    divisor = int(input("Divisor (no 0): "))
+print("El resultado de la división es", dividendo / divisor)
+
+
+
+#Programa que calcula la suma de 135 y 768
+suma = 905
+n = int(input("Dame la suma de 135 y 768: "))
+
+while (n != 905) :
+    n = int(input("Dame otro número: "))
+print("¡Conseguido!")
+
+
+clave = int(input("Dame una clave: "))
+
+"""
+
+#Programa de obtención de usuario y contraseña
+usuario = admin
+code = int(input("Dame un usuario: "))
+
+while (code != admin) :
+    code = int(input("Usuario erróneo, inténtalo de nuevo: "))
+print("Ahora introduce la contraseña")
+
