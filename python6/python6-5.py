99685bf8188d4b1293959bcb04827ef86c805f2e
nadiaexp <nadexpmoy@alu.edu.gva.es>
2025-01-22 13:50:07 +0100

    IES22E

--- python6/python6-5.py
+++ python6/python6-5.py
@@ -13,22 +13,50 @@ opción hasta que el usuario elija salir.
 n = int(input("Dime un número: "))
 m = int(input("Dime otro número: "))
 
+print("1 - Mostrar la suma de los dos números")
+print("2 - Mostrar la resta del primero menos el segundo")
+print("3 - Mostrar la multiplicación de los dos números")
+print("4 - Salir del programa") 
+
 o = input("Elija una de las 4 opciones: ")
 
-while (o != 1) or (o != 2) or (o != 3) or (o != 4) :
-    print("Incorrecto, inténtalo de nuevo: ")
+"""
+while (o == 1) :
+    o = float(input("1 - Mostrar la suma de los números"))
+print(n + m)
 
-if (o == 1) :
-    print("1 - Mostrar la suma de los dos números")
+while (o == 2) :
+    o = float(input("2 - Mostrar la resta del primero menos el segundi"))
+print(n - m)
 
-if (o == 2) :
-    print("2 - Mostrar la resta del primero menos el segundo")
+while (o == 3) :
+    o = float(input("3 - Mostrar la multiplicación de los dos números"))
+print(n * m)
 
-if (o == 3) :
-    print("3 - Mostrar la multiplicación de los dos números")
+while (o == 4) :
+    o = float(input("4 - Salir del programa"))
+print("Apagado.")
+
+
+while (o == 1 or 2 or 3 or 4) :
     
-if (o == 4) :
-    print("4 - Salir del programa") 
+    if (o == 1) :
+        print(n + m)
+    if (o == 2) :
+        print(n - m)
+    if (o == 3) :
+        print(n * m)
+    if (o == 4) :
+        print("Fin")
 
+"""
 
-    
+while (o >= 1) or (o <= 4) :
+    if o == 1 :
+        print(n + m)
+    if o == 2 :
+        print(n - m)
+    if o == 3 :
+        print(n * m)
+    if o == 4 :
+        print("Fin")
