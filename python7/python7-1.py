e6d6e50b9277901136c818522280c64ce9a14a4c
nadiaexp <nadexpmoy@alu.edu.gva.es>
2025-01-28 09:50:25 +0100

    IES28E

---
+++ python7/python7-1.py
@@ -0,0 +1,12 @@
+"""
+ Crea un programa que pida al usuario que introduzca su nombre y un
+número entero y escriba su nombre en pantalla tantas veces como indique ese
+número entero.
+
+"""
+
+u = input("Dime tu nombre: ")
+n = input("Dime un número entero: ")
+
+for n = n 
+    print(n)
